757502e5c1149a97dd97ab9e6e87529ba7b02cd1
omeros <omerosorama@gmail.com>
2019-05-21 00:01:28 +0300

    Update Start.js

--- Start.js
+++ Start.js
@@ -4,24 +4,24 @@
 
 
 <form>
- 1. The question will be here <br>
+ 1. The first question will be here <br>
   
-  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="1_1">2_1<br>
-  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="1_2">2_2<br>
-  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="1_3">2_3<br>
-  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="1_4">2_4<br>        
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="1_1">1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="1_2">2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="1_3">3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="1_4">4<br>        
 </form>
 
 <form>
 
 <form>
-  2.The question will be here<br>
+  2.The second question will be here<br>
   aaaaaa
-  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="red">1_1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="2_1">1<br>
   bbbbbb
-  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="blue">1_2<br>
-  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="black">1_3<br>
-  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="white">1_4<br>        
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="2_2">2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="2_3">3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="2_4">4<br>        
 </form>
 
 
@@ -121,19 +121,19 @@
 <form>
   13.The question will be here <br>
   
-  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="2_1">13_1<br>
-  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="2_2">13_2<br>
-  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="2_3">13_3<br>
-  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="2_4">13_4<br>        
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="13_1">13_1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="13_2">13_2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="13_3">13_3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="13_4">13_4<br>        
 </form>
 
 <form>
   14.The question will be here <br>
   
-  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="2_1">14_1<br>
-  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="2_2">14_2<br>
-  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="2_3">14_3<br>
-  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="2_4">14_4<br>        
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="14_1">14_1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="14_2">14_2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="14_3">14_3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="14_4">14_4<br>        
 </form>
 
 
@@ -167,20 +167,268 @@ function myFunction2_1()
    answered[1]=1;
    document.write(answered[0]);
 }
-
 function myFunction2_2()
 {
    answered[1]=2;
    document.write(answered[0]);
 }
-
 function myFunction2_3()
 {
    answered[1]=3;
    document.write(answered[0]);
 }
-
 function myFunction2_4()
+{
+   answered[1]=4;
+   document.write(answered[0]);
+}
+
+function myFunction3_1()
+{
+   answered[1]=1;
+   document.write(answered[0]);
+}
+function myFunction3_2()
+{
+   answered[1]=2;
+   document.write(answered[0]);
+}
+function myFunction3_3()
+{
+   answered[1]=3;
+   document.write(answered[0]);
+}
+function myFunction3_4()
+{
+   answered[1]=4;
+   document.write(answered[0]);
+}
+
+function myFunction4_1()
+{
+   answered[1]=1;
+   document.write(answered[0]);
+}
+function myFunction4_2()
+{
+   answered[1]=2;
+   document.write(answered[0]);
+}
+function myFunction4_3()
+{
+   answered[1]=3;
+   document.write(answered[0]);
+}
+function myFunction4_4()
+{
+   answered[1]=4;
+   document.write(answered[0]);
+}
+
+function myFunction5_1()
+{
+   answered[1]=1;
+   document.write(answered[0]);
+}
+function myFunction5_2()
+{
+   answered[1]=2;
+   document.write(answered[0]);
+}
+function myFunction5_3()
+{
+   answered[1]=3;
+   document.write(answered[0]);
+}
+function myFunction5_4()
+{
+   answered[1]=4;
+   document.write(answered[0]);
+}
+
+function myFunction6_1()
+{
+   answered[1]=1;
+   document.write(answered[0]);
+}
+function myFunction6_2()
+{
+   answered[1]=2;
+   document.write(answered[0]);
+}
+function myFunction6_3()
+{
+   answered[1]=3;
+   document.write(answered[0]);
+}
+function myFunction6_4()
+{
+   answered[1]=4;
+   document.write(answered[0]);
+}
+
+function myFunction7_1()
+{
+   answered[1]=1;
+   document.write(answered[0]);
+}
+function myFunction7_2()
+{
+   answered[1]=2;
+   document.write(answered[0]);
+}
+function myFunction7_3()
+{
+   answered[1]=3;
+   document.write(answered[0]);
+}
+function myFunction7_4()
+{
+   answered[1]=4;
+   document.write(answered[0]);
+}
+
+function myFunction8_1()
+{
+   answered[1]=1;
+   document.write(answered[0]);
+}
+function myFunction8_2()
+{
+   answered[1]=2;
+   document.write(answered[0]);
+}
+function myFunction8_3()
+{
+   answered[1]=3;
+   document.write(answered[0]);
+}
+function myFunction8_4()
+{
+   answered[1]=4;
+   document.write(answered[0]);
+}
+
+function myFunction9_1()
+{
+   answered[1]=1;
+   document.write(answered[0]);
+}
+function myFunction9_2()
+{
+   answered[1]=2;
+   document.write(answered[0]);
+}
+function myFunction9_3()
+{
+   answered[1]=3;
+   document.write(answered[0]);
+}
+function myFunction9_4()
+{
+   answered[1]=4;
+   document.write(answered[0]);
+}
+
+function myFunction10_1()
+{
+   answered[1]=1;
+   document.write(answered[0]);
+}
+function myFunction10_2()
+{
+   answered[1]=2;
+   document.write(answered[0]);
+}
+function myFunction10_3()
+{
+   answered[1]=3;
+   document.write(answered[0]);
+}
+function myFunction10_4()
+{
+   answered[1]=4;
+   document.write(answered[0]);
+}
+function myFunction11_1()
+{
+   answered[1]=1;
+   document.write(answered[0]);
+}
+function myFunction11_2()
+{
+   answered[1]=2;
+   document.write(answered[0]);
+}
+function myFunction11_3()
+{
+   answered[1]=3;
+   document.write(answered[0]);
+}
+function myFunction11_4()
+{
+   answered[1]=4;
+   document.write(answered[0]);
+}
+
+function myFunction12_1()
+{
+   answered[1]=1;
+   document.write(answered[0]);
+}
+function myFunction12_2()
+{
+   answered[1]=2;
+   document.write(answered[0]);
+}
+function myFunction12_3()
+{
+   answered[1]=3;
+   document.write(answered[0]);
+}
+function myFunction12_4()
+{
+   answered[1]=4;
+   document.write(answered[0]);
+}
+
+function myFunction13_1()
+{
+   answered[1]=1;
+   document.write(answered[0]);
+}
+function myFunction13_2()
+{
+   answered[1]=2;
+   document.write(answered[0]);
+}
+function myFunction13_3()
+{
+   answered[1]=3;
+   document.write(answered[0]);
+}
+function myFunction13_4()
+{
+   answered[1]=4;
+   document.write(answered[0]);
+}
+
+function myFunction14_1()
+{
+   answered[1]=1;
+   document.write(answered[0]);
+}
+function myFunction14_2()
+{
+   answered[1]=2;
+   document.write(answered[0]);
+}
+function myFunction14_3()
+{
+   answered[1]=3;
+   document.write(answered[0]);
+}
+function myFunction14_4()
 {
    answered[1]=4;
    document.write(answered[0]);
