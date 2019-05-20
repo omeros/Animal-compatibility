597ae34db0d307b4155533e829b365c355a926b5
omeros <omerosorama@gmail.com>
2019-05-20 23:46:13 +0300

    Create Start.js

---
+++ Start.js
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<form>
+ 1. The question will be here <br>
+  
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="1_1">2_1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="1_2">2_2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="1_3">2_3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="1_4">2_4<br>        
+</form>
+
+<form>
+
+<form>
+  2.The question will be here<br>
+  aaaaaa
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="red">1_1<br>
+  bbbbbb
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="blue">1_2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="black">1_3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="white">1_4<br>        
+</form>
+
+
+
+
+<form>
+  3.The question will be here <br>
+  
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="3_1">2_1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="3_2">2_2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="3_3">2_3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="3_4">2_4<br>        
+</form>
+
+
+<form>
+ 4.The question will be here <br>
+  
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="4_1">1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="4_2">2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="4_3">3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="4_4">4<br>        
+</form>
+
+<form>
+  5.The question will be here <br>
+  
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="5_1">5_1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="5_2">5_2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="5_3">5_3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="5_4">5_4<br>        
+</form>
+
+<form>
+  6.The question will be here <br>
+  
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="6_1">1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="6_2">2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="6_3">3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="6_4">4<br>        
+</form>
+
+<form>
+  7. The question will be here <br>
+  
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="7_1">1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="7_2">2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="7_3">3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="7_4">4<br>        
+</form>
+
+<form>
+  8.The question will be here <br>
+  
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="8_1">8_1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="8_2">8_2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="8_3">8_3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="8_4">8_4<br>        
+</form>
+
+<form>
+  9.The question will be here <br>
+  
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="9_1">9_1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="9_2">9_2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="9_3">9_3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="9_4">9_4<br>        
+</form>
+
+<form>
+  10.The question will be here <br>
+  
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="10_1">10_1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="10_2">10_2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="10_3">10_3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="10_4">10_4<br>        
+</form>
+
+<form>
+  11. The question will be here <br>
+  
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="11_1">11_1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="11_2">11_2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="11_3">11_3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="11_4">11_4<br>        
+</form>
+
+
+  12.The question will be here <br>
+  
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="12_1">12_1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="12_2">12_2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="12_3">12_3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="12_4">12_4<br>        
+</form>
+
+<form>
+  13.The question will be here <br>
+  
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="2_1">13_1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="2_2">13_2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="2_3">13_3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="2_4">13_4<br>        
+</form>
+
+<form>
+  14.The question will be here <br>
+  
+  <input type="radio"  onclick="if(this.checked){myFunction1_1()}" name="colors" id="2_1">14_1<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_2()}" name="colors" id="2_2">14_2<br>
+  <input type="radio"  onclick="if(this.checked){myFunction1_3()}" name="colors" id="2_3">14_3<br>
+  <input type="radio" onclick="if(this.checked){myFunction1_4()}"  name="colors" id="2_4">14_4<br>        
+</form>
+
+
+<script>
+var answered = [];
+
+function myFunction1_1()
+{
+   answered[0]=1;
+   document.write(answered[0]);
+}
+function myFunction1_2()
+{
+   answered[0]=2;
+   document.write(answered[0]);
+}
+function myFunction1_3()
+{
+   answered[0]=3;
+   document.write(answered[0]);
+}
+function myFunction1_4()
+{
+   answered[0]=4;
+   document.write(answered[0]);
+}
+
+
+function myFunction2_1()
+{
+   answered[1]=1;
+   document.write(answered[0]);
+}
+
+function myFunction2_2()
+{
+   answered[1]=2;
+   document.write(answered[0]);
+}
+
+function myFunction2_3()
+{
+   answered[1]=3;
+   document.write(answered[0]);
+}
+
+function myFunction2_4()
+{
+   answered[1]=4;
+   document.write(answered[0]);
+}
+</script>
+
+</body>
+</html>
